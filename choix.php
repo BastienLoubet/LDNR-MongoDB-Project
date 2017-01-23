e5cc47936792f8aee093cf8f7aee82840ca78252
BastienLoubet <loubetbastien@yahoo.fr>
2017-01-23 16:24:20 +0100

    modif choix.php

--- choix.php
+++ choix.php
@@ -1,6 +1,6 @@
 <?php
 /*
-Fichier template
+ contient un liens pour revenir vers select.php avec les choix envoyer par select.php et fabrique le formulaire.
 */
 require_once('./php/basic_functions.php');
 //Avant d'envoyer le premier header http
@@ -18,25 +18,6 @@ say('Mon super message de debug !');
 error('Mon super message d\'erreur !');
 
 echo html('p','Voici un super parragraphe.');
-
-//On commence le formulaire
-echo "<form id='inscriptionForm' action='' method='post'>
-    <fieldset>
-    <legend>Formulaire</legend>";
-//On construit les autre inputs
-function make_input(string $type,string $name,string $label=''){
-    if(isset($_POST[$name]) && $_POST[$name]!='') $val="value='".htmlspecialchars($_POST[$name])."'";
-    else $val='';
-    echo "<label class='firstCol'>
-    <input type='$type' name='$name' $val";
-    if ($label!='') echo " placeholder=$label";    
-    echo "></label>
-    <br>";
-}
-make_input('text','phone_number',"Numero de telephone");
-
-echo "<input type='submit' name='B1' id='submit' value='Envoyer' >";
-echo '</fieldset></form>';
 
 
 make_html_end();
