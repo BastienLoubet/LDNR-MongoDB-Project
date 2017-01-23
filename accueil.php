b8707b76b1a2a510e2f5e9b375c6f7d783da11bc
MichelFG <michelfigue@outlook.fr>
2017-01-23 17:11:21 +0100

    css
    commentaire

--- accueil.php
+++ accueil.php
@@ -6,4 +6,28 @@ le nom de la ville (mention obligatoire),<input ’nom’
 le département ou se situe la ville (optionnel),<input name=’dept’
 la région ou se situe la ville (optionnel),<input name=’region’
 un bouton de soumission du formulaire.
-*/
+*/ 
+
+?>
+
+
+
+
+
+    <html>
+    <fieldset>
+        <div class="frm">
+            <form method="get" action="#">
+                <input type="text" name="villes" placeholder="Villes"></input>
+                <br>
+                <input type="text" name="dept" placeholder="Département"></input>
+                <br>
+                <input type="text" name="region" placeholder="Région"></input>
+                <br>
+                <input class="Cnx" type="submit" name="Connexion"></input>
+            </form>
+        </div>
+    </fieldset>
+
+
+    </html>
