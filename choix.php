5de12fdd91d2e3bc5a828a96b9808aa1fe5096b9
FabriceHOOGE <fabrice.hooge@outlook.fr>
2017-01-23 17:30:45 +0100

    Ajuot de code pour la page choix

--- choix.php
+++ choix.php
@@ -8,16 +8,51 @@ et fabrique le formulaire.
 require_once('./php/basic_functions.php');
 //Avant d'envoyer le premier header http
 
-
 //On cree le html
 make_html_start('Template','./css/template.css');
 
-make_nav_bar();
-
 say('Mon super message de debug !');
 error('Mon super message d\'erreur !');
+$param = NULL;
+$test = true;
+if(isset($_GET[]))
+{
+    $param = $_GET;
+}
+else
+{
+    if($test)
+    {
+        $param = [
+            ville1 => [ville => 'Paris', dept => '75', region => 'Ile-De-France' ],
+            ville2 => [ville => 'Paris', dept => '75', region => 'Ile-De-France' ],
+            ville3 => [ville => 'Paris', dept => '75', region => 'Ile-De-France' ]
+        ];
+    }
+}
 
-echo html('p','Voici un super parragraphe.');
+echo '<form method="GET" action="">';
+echo '<fieldset>';
+echo '<legend>Choisissez une ville''</legend>';
+if(!$test)
+{   
+    foreach($param as $vals)
+    {
+        foreach(vals as $key => $val)
+        {
+            echo '<label>ville: '.$val;
+            echo '<input type="radio" name="choix_ville" value='.$val.'>';
+            echo '</label>';
+        }
+    }       
+}
+else{
+    error('la variable $_GET n\'existe pas.');
+}
+echo '</fieldset>';
+echo '</form>';
+
 
 
 make_html_end();
+
