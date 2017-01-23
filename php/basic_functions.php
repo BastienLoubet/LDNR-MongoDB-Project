d87636a4d53c1cbc0b2851619f805a33f527cb97
BastienLoubet <loubetbastien@yahoo.fr>
2017-01-23 14:36:18 +0100

    creation fichier template et style

--- php/basic_functions.php
+++ php/basic_functions.php
@@ -1,9 +1,33 @@
 <?php
 header('Content-Type: text/html; charset=utf-8');
 
+//FONCTIONS DE DEBUG 
 //automatise la mise en forme du print_r
 function bprint_r($str) {
     echo '<pre>'.print_r($str,true).'</pre>';
+}
+
+//cree un message dans un paragraphe
+function say(string $str) {
+    echo "<p>$str</p>";
+}
+
+//cree un message dans un paragraphe
+function error(string $str) {
+    echo "<p class=\"error\">$str</p>";
+}
+
+//FONCTIONS pour creer le html
+//Pour imbrique des elements html entre eux sans risque de faire une erreur sur une balise fermante.
+function html(string $bal,string $str,string $class='',string $sOptionSup=''){
+    if($class == '') $classstr = '';
+    else $classstr = "class='".htmlspecialchars($class)."'";
+    return "<$bal $classstr $sOptionSup>".$str."</$bal>";
+}
+
+//
+function make_nav_bar(){
+    echo html('div','Ma barre de navigation','navbar');
 }
 
 //Cree le header standard de tout mes fichiers html
@@ -14,6 +38,7 @@ function make_html_start(string $title, string $cssfile='./css/style.css') {
     echo '<meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">';
     echo "
+        <link rel=\"stylesheet\" href=\"./css/style.css\">
         <link rel=\"stylesheet\" href=\"$cssfile\">
         </head><body>";
 }
@@ -21,16 +46,4 @@ function make_html_start(string $title, string $cssfile='./css/style.css') {
 //ferme les balises ouverte par make_html_start
 function make_html_end(){
     echo '</body></html>';
-}
-
-//cree un message dans un paragraphe
-function say(string $str) {
-    echo "<p>$str</p>";
-}
-
-//Pour imbrique des elements html entre eux sans risque de faire une erreur sur une balise fermante.
-function html(string $str,string $bal='p',string $class=''){
-    if($class == '') $classstr = '';
-    else $classstr = "class='".htmlspecialchars($class)."'";
-    return "<$bal>".$str."</$bal>";
 }
