3b12c51b9e8e81400f63c47f68ab3ba943a9560a
FabriceHOOGE <fabrice.hooge@outlook.fr>
2017-01-24 00:51:51 +0100

    Evolution choix.php, création du fichier choix.css

--- choix.php
+++ choix.php
@@ -34,7 +34,7 @@ else //si la variable $_GET est vide, on vérifie si le script est lancé en mod
 //affichage du formulaire pour choisir la bonne ville.
 
 echo '<form method="GET" action="">'."\n";
-echo '<fieldset>'."\n";
+echo '<fieldset class="fieldset">'."\n";
 echo '<legend>Choisissez une ville</legend>'."\n";
 
 if(!empty($param))
@@ -42,18 +42,17 @@ if(!empty($param))
     
     foreach($param as $vals)
     {
-        echo '<p>'."\n";
-        echo '<input type="radio" name="choix_ville" value='.implode('_',$vals).'>'."\n"; //affichage des boutons radio la valeur est la concaténation des chaînes villes, département et région
-        echo implode(' - ',$vals); //affichage du texte
-        echo '</label>'."\n";
-        echo '</p>'."\n";
+        echo "<div>"."\n";
+        echo '<input class="submit" type="submit" name="choix_ville" value='.implode('_',$vals).'>'."\n"; //affichage des boutons radio la valeur est la concaténation des chaînes villes, département et région
+        //echo implode(' - ',$vals); //affichage du texte
+        echo '</div>'."\n";
     }
            
 }
 else{
     error('la variable $param est vide.'); //affichage d'un message d'erreur si la variable $param ne contient aucune information
 }
-echo '<input type="submit" name="Valider"/>'."\n";
+//echo '<input id="submit" type="submit" name="Valider" value="Valider"/>'."\n";
 echo '</fieldset>'."\n";
 echo '</form>'."\n";
 
