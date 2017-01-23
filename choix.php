10b3f45480be43a6c0f59de10efb3f6ceab20c92
FabriceHOOGE <fabrice.hooge@outlook.fr>
2017-01-23 16:33:44 +0100

    fusionné choix.php
    test de fusion de fichier

--- choix.php
+++ choix.php
@@ -1,16 +1,12 @@
 <?php
 /*
-<<<<<<< HEAD
 Si l'utilisateur souhaite une ville dont le nom existe en plusieurs endroits,
 cette page affiche les choix disponibles pour ce nom de ville ainsi que le département et la région.
-=======
->>>>>>> origin/master
+Contient un liens pour revenir vers select.php avec les choix envoyer par select.php 
+et fabrique le formulaire.
 */
 require_once('./php/basic_functions.php');
 //Avant d'envoyer le premier header http
-
-
-
 
 
 //On cree le html
@@ -23,9 +19,5 @@ error('Mon super message d\'erreur !');
 
 echo html('p','Voici un super parragraphe.');
 
-//On commence le formulaire
-    <fieldset>
-function make_input(string $type,string $name,string $label=''){
-    if(isset($_POST[$name]) && $_POST[$name]!='') $val="value='".htmlspecialchars($_POST[$name])."'";
 
 make_html_end();
