cfe4c5e5e893b93dd47aec1c0ec564d784ddbb41
MichelFG <michelfigue@outlook.fr>
2017-01-24 15:15:36 +0100

    navbar

--- accueil.php
+++ accueil.php
@@ -8,8 +8,9 @@ la région ou se situe la ville (optionnel),<input name=’region’
 un bouton de soumission du formulaire.
 */ 
 require_once('./php/basic_functions.php');
-make_nav_bar();
+
 make_html_start("Page d'accueil",'./css/accueil.css'); 
+make_nav_bar();
 echo '<div class="logo">';
 echo '<img src="./img/newlog1.png" alt="carte" class="cart">';
 echo '</div>';
@@ -30,9 +31,9 @@ echo '<input class="submit" type="submit" name="accueil" value=Rechercher>';
 echo '</input>';
 echo '</form>';
 echo '</div>';
-echo '<div class="ctfr">';
+/*echo '<div class="ctfr">';
 echo '<img src="france.png" alt="carte" class="carte">';
-echo '</div>';
+echo '</div>';*/
 echo '</fieldset>';
 echo '</div>';
 
