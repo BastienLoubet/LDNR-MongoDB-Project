ea4d5562de73c50b8abc665bcd2b9b5635e7770a
BastienLoubet <loubetbastien@yahoo.fr>
2017-01-24 15:47:02 +0100

    rerere correction de make_html_start pour les /n

--- php/basic_functions.php
+++ php/basic_functions.php
@@ -41,14 +41,14 @@ function make_nav_bar(){
 
 //Cree le header standard de tout mes fichiers html
 function make_html_start(string $title, string $cssfile='') {
-    echo '<!DOCTYPE html>/n
-        <html>/n<head>/n';
-    echo "<title> $title </title>/n";
-    echo '<meta charset="UTF-8">/n
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">/n';
-    echo "<link rel=\"stylesheet\" href=\"./css/style.css\">/n";
+    echo "<!DOCTYPE html>\n
+        <html>\n<head>\n";
+    echo "<title> $title </title>\n";
+    echo "<meta charset=\"UTF-8\">\n
+        <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">\n";
+    echo "<link rel=\"stylesheet\" href=\"./css/style.css\">\n";
     if ($cssfile !='') echo "<link rel=\"stylesheet\" href=\"$cssfile\">";
-    echo "</head><body>/n";
+    echo "</head><body>\n";
 }
 
 //ferme les balises ouverte par make_html_start
