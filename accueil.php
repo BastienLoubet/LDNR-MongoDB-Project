68b58d95b2f45589a8a1d2d2cdf8a4f5a54abd55
BastienLoubet <loubetbastien@yahoo.fr>
2017-01-26 14:45:06 +0100

    update autocompletation

--- accueil.php
+++ accueil.php
@@ -8,6 +8,11 @@ la région ou se situe la ville (optionnel),<input name=’region’
 un bouton de soumission du formulaire.
 */ 
 require_once('./php/basic_functions.php');
+function InputGeneratorAutocomplete(string $inputId,string $placeholder,string $divResultId,string $collectionName){
+    InputGenerator ($inputId , $placeholder,"id='$inputId' autocomplete='off'");
+    echo "<div id='$divResultId' class='results'></div>";
+    echo "<script src='./php/autocompletion.php?inputId=$inputId&collectionName=$collectionName&divResultId=$divResultId'></script>";
+}
 
 make_html_start("Page d'accueil",'./css/accueil.css'); 
 make_nav_bar();
@@ -22,9 +27,10 @@ echo 'Cherchez une ville';
 echo '</h1>';
 echo '<form method="get" action="./select.php">';
 
-InputGenerator ("villes" , "Villes","id='search' autocomplete='off'");
-echo '<div id="results" class="results"></div>';
-InputGenerator ("dept" , "Département","id='searchDept' autocomplete='off'");
+/*InputGenerator ("villes" , "Villes","id='villes' autocomplete='off'");
+echo '<div id="resultsVilles" class="results"></div>';*/
+InputGeneratorAutocomplete('villes',"Villes",'resultsVilles','villes');
+InputGenerator ("dept" , "Département","id='dept' autocomplete='off'");
 echo '<div id="resultsDept" class="results"></div>';
 InputGenerator ("region" , "Régions");
 echo '<input class="submit" type="submit" name="accueil" value=Rechercher>';
@@ -36,5 +42,5 @@ echo '</div>';
 echo '</fieldset>';
 echo '</div>';
 
-echo '<script src="./js/autocompletion.js"></script>';
+echo '<script src="./php/autocompletion.php?inputId=villes&collectionName=villes&divResultId=resultsVilles"></script>';
 make_html_end();
