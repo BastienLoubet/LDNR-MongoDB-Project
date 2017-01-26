23910159d88631dcd4263378545d7f8653d8ccd6
BastienLoubet <loubetbastien@yahoo.fr>
2017-01-26 15:51:38 +0100

    bug connection

--- testSession2.php
+++ testSession2.php
@@ -2,6 +2,6 @@
 require_once('./php/basic_functions.php');
 session_start();
 make_html_start('Test');
-$_SESSION['connect']='admin';
+//$_SESSION['connect']='admin';
 bprint_r($_SESSION);
 make_html_end();
